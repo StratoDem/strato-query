ca4b2655475dcad7d2c503c199d4ceca532cc621
Eric <eric@stratodem.com>
2018-12-26 12:13:03 -0500

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -8,3 +8,20 @@ Notes :
 
 December 26, 2018
 """
+
+from setuptools import setup
+
+setup(
+    name='strato_query',
+    version='0.1.0',
+    author='Michael Clawar, Raaid Arshad, Eric Linden',
+    author_email='tech@stratodem.com',
+    packages=[
+        'strato_query',
+        'strato_query.base_API_query',
+        'strato_query.standard_filters',
+    ],
+    license='MIT',
+    description='StratoDem DB API tools',
+    url='https://github.com/StratoDem/strato-query',
+)
