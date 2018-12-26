47937d59f1724fc6e30cefc2876ff122aaec1f8d
Eric <eric@stratodem.com>
2018-12-26 11:17:57 -0500

    Add config with some basic values

---
+++ config/__init__.py
@@ -0,0 +1,25 @@
+"""
+StratoDem Analytics : __init__.py
+Principal Author(s) : Eric Linden
+Secondary Author(s) : 
+Description :
+
+Notes : 
+
+December 26, 2018
+"""
+
+
+check_global_configure = True
+check_configure = True
+
+
+class ProfileWarning(Warning):
+    """Normal ImportWarning is ignored by default (and other sub-modules use it)"""
+    pass
+
+
+user = 'NOT_SPECIFIED'
+DEBUG_EMAIL = 'tech@stratodem.com'
+DEBUG = False
+debug_token = 'debug-token'
