32bb313d9fe68fd947ee5f8532f9410acc533fe6
Eric <eric@stratodem.com>
2018-12-26 11:18:30 -0500

    Add constants file with URL for API

---
+++ strato_query/core/constants.py
@@ -0,0 +1,12 @@
+"""
+StratoDem Analytics : constants
+Principal Author(s) : Eric Linden
+Secondary Author(s) : 
+Description :
+
+Notes : 
+
+December 26, 2018
+"""
+
+api_url = 'https://api.stratodem.com/api'
