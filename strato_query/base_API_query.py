3f840652c53e4e4f12b3ea24c99a50b5e3333c5e
Eric <eric@stratodem.com>
2019-01-04 15:10:42 -0500

    Add new classes to `__all__`

--- strato_query/base_API_query.py
+++ strato_query/base_API_query.py
@@ -23,7 +23,7 @@ from strato_query.core import constants as cc
 
 
 __all__ = [
-    'APIQueryParams', 'BaseAPIQuery',
+    'APIQueryParams', 'BaseAPIQuery', 'APIMeanQueryParams', 'APIMedianQueryParams',
 ]
 
 API_TOKEN = os.environ.get('API_TOKEN', sdc.debug_token)
