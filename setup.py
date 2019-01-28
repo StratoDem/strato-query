f5646927e196c915b32dc725f718108af2783471
Eric <eric@stratodem.com>
2019-01-28 12:25:44 -0500

    Update version and CHANGELOG

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='strato_query',
-    version='2.5.8',
+    version='2.6.0',
     author='Michael Clawar, Raaid Arshad, Eric Linden',
     author_email='tech@stratodem.com',
     packages=[
