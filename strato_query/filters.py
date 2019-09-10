492680dba9faaf9839474a9fc5f42173c701c6e0
Eric <eric@stratodem.com>
2019-09-10 13:28:05 -0400

    Update filter and docstring
    Filter now expects to be provided a geometry instead of the pieces
    needed to reconstruct the geometry

--- strato_query/filters.py
+++ strato_query/filters.py
@@ -202,56 +202,41 @@ class IntersectsFilter(BaseFilter):
 class OverlapsFilter(BaseFilter):
     def __init__(self,
                  var: str,
-                 latitude: float,
-                 longitude: float,
-                 detailed_type: str,
-                 miles: Optional[Union[int, float]] = None,
-                 minutes: Optional[Union[int, float]] = None,
-                 with_traffic: Optional[bool] = False,
-                 start_time: Optional[str] = None):
+                 geometry: str,
+                 miles: Optional[Union[int, float]] = 0):
         """
-        Filter a query by geometries overlapping the market shape, specified by market center and
-        buffer details (miles, time traveled by auto or foot)
+        Filter a query by geometries overlapping the market shape and surrounding buffer
 
         Parameters
         ----------
         var: str
             The name of the geometry column against which this filter will check
-        latitude: float
-            Center latitude
-        longitude: float
-            Center longitude
-        detailed_type: str
-            One of the following buffers from the latitude/longitude center:
-            - 'overlaps_mile_radius': use a mile distance buffer
-            - 'overlaps_drivetime': use a drivetime buffer, can be with or without traffic
-            - 'overlaps_walktime': use a walktime buffer
+        geometry: str
+            The geojson for the market shape. Can be a single point, or a polygon
+
+            Examples
+                geometry='{"type": "Point", "coordinates": [-96.91386923077042, 33.08536026887235]}'
+                geometry='{"type": "Polygon",
+                           "coordinates": [[[-71.06944084167479, 42.35721199960259],
+                                            [-71.07012748718262, 42.34205151655285],
+                                            [-71.03416442871094, 42.342685919682445],
+                                            [-71.03579521179199, 42.35537263800411],
+                                            [-71.05021476745605, 42.3647591610627],
+                                            [-71.06944084167479, 42.35721199960259]]]}'
+
+
         miles: Optional[Union[int, float]]
-            Default None. Miles traveled from latitude/longitude center
-        minutes: Optional[Union[int, float]]
-            Default None. Minutes drive from latitude/longitude center
-        with_traffic: Optional[bool]
-            Default False. Use traffic estimates to compute the drive time area?
-        start_time: Optional[str]
-            Default None. The departure time for the drivetime, used in concert with "with_traffic"
-            set to True
-                Example: "2019-05-25T18:00:00"
+            Default 0. The number of miles out from the given market geometry. If that geometry
+            is a point, this value should be set explicitly. If the geometry is a polygon, this
+            value can be omitted.
         """
-        assert detailed_type in {'overlaps_mile_radius', 'overlaps_drivetime', 'overlaps_walktime'}
-        assert isinstance(latitude, float)
-        assert isinstance(longitude, float)
+        assert isinstance(var, str)
+        assert isinstance(geometry, str)
         assert miles is None or isinstance(miles, (int, float))
-        assert minutes is None or isinstance(minutes, (int, float))
-        assert isinstance(with_traffic, bool)
-        assert start_time is None or isinstance(start_time, str)
 
         super().__init__(
-            filter_type=detailed_type,
+            filter_type='overlaps',
             filter_variable=var,
             filter_value=dict(
-                latitude=latitude,
-                longitude=longitude,
-                miles=miles,
-                minutes=minutes,
-                traffic='enabled' if with_traffic else 'disabled',
-                start_time=start_time))
+                geometry=geometry,
+                miles=miles))
