edae9530938690feee251b5c73ec69f9eca2eb6d
Eric <eric@stratodem.com>
2019-11-12 11:21:30 -0500

    Add constant for buffer filter names

--- strato_query/constants.py
+++ strato_query/constants.py
@@ -12,3 +12,21 @@ December 26, 2018
 API_URL = 'https://api.stratodem.com/api'
 
 MAX_RETRIES = 3
+
+SPECIAL_FILTER_SET = frozenset({
+    'mile_radius',
+    'mile_radius_simple',
+    'mile_radius_unweighted',
+    'drivetime',
+    'drivetime_simple',
+    'drivetime_unweighted',
+    'drivetime_destination',
+    'drivetime_destination_simple',
+    'drivetime_destination_unweighted',
+    'walktime',
+    'walktime_unweighted',
+    'walktime_destination',
+    'walktime_destination_unweighted',
+    'intersects',
+    'intersects_weighted',
+})
