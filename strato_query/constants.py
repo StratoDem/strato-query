d2b270ca496eecd416eb7e715575a45c4b3d29b7
Eric <eric@stratodem.com>
2019-11-12 11:28:30 -0500

    Add mapping from filter type to func

--- strato_query/constants.py
+++ strato_query/constants.py
@@ -9,6 +9,8 @@ Notes :
 December 26, 2018
 """
 
+from types import MappingProxyType
+
 API_URL = 'https://api.stratodem.com/api'
 
 MAX_RETRIES = 3
@@ -29,4 +31,22 @@ SPECIAL_FILTER_SET = frozenset({
     'walktime_destination_unweighted',
     'intersects',
     'intersects_weighted',
+})
+
+MAP_SPECIAL_FILTER_TO_FUNC = MappingProxyType({
+    'mile_radius': 'mile_radius_filter',
+    'mile_radius_simple': 'mile_radius_filter',
+    'mile_radius_unweighted': 'mile_radius_filter',
+    'drivetime': 'drivetime_filter',
+    'drivetime_simple': 'drivetime_filter',
+    'drivetime_unweighted': 'drivetime_filter',
+    'drivetime_destination': 'drivetime_filter',
+    'drivetime_destination_simple': 'drivetime_filter',
+    'drivetime_destination_unweighted': 'drivetime_filter',
+    'walktime': 'walktime_filter',
+    'walktime_unweighted': 'walktime_filter',
+    'walktime_destination': 'walktime_filter',
+    'walktime_destination_unweighted': 'walktime_filter',
+    'intersects': 'intersects_filter',
+    'intersects_weighted': 'intersects_filter',
 })
